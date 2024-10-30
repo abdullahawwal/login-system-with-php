deda3614316a7c745cf401380a6607d8f8ce3f8e
Abdullah Awwal <contact@abdullahawwal.com>
2024-10-30 07:55:19 +0000

    finalized

--- Admin.php
+++ Admin.php
@@ -22,103 +22,6 @@ class Admin {
     }
 
 //****#####--Database Cunnection Function End--####*** 
-//****#####--Common Method Function start--####***
-    public function getOneCol($col, $tbl, $comCol, $comVal) {
-        $db_connect = $this->__construct();
-
-        $sql = "SELECT $col FROM $tbl WHERE $comCol='$comVal' ";
-        if (mysqli_query($db_connect, $sql)) {
-            $result = mysqli_query($db_connect, $sql);
-            $row = mysqli_fetch_array($result);
-            return $row[$col];
-        } else {
-            die('Query problem' . mysqli_error($db_connect));
-        }
-    }
-    public function getOneColBranch($col, $tbl, $comCol, $comVal,$branch) {
-        $db_connect = $this->__construct();
-
-        $sql = "SELECT $col FROM $tbl WHERE $comCol='$comVal' AND branch_id='$branch'";
-        if (mysqli_query($db_connect, $sql)) {
-            $result = mysqli_query($db_connect, $sql);
-            $row = mysqli_fetch_array($result);
-            return $row[$col];
-        } else {
-            die('Query problem' . mysqli_error($db_connect));
-        }
-    }
-        public function getOneColDist($col, $tbl, $comCol, $comVal,$dist) {
-        $db_connect = $this->__construct();
-
-        $sql = "SELECT $col FROM $tbl WHERE $comCol='$comVal' AND distributor_id='$dist'";
-        if (mysqli_query($db_connect, $sql)) {
-            $result = mysqli_query($db_connect, $sql);
-            $row = mysqli_fetch_array($result);
-            return $row[$col];
-        } else {
-            die('Query problem' . mysqli_error($db_connect));
-        }
-    }
-    public function getOneColRand($col, $tbl) {
-        $db_connect = $this->__construct();
-
-        $sql = "SELECT $col FROM $tbl WHERE user_id = '$_SESSION[user_id]' order by id desc limit 1";
-        if (mysqli_query($db_connect, $sql)) {
-               $query_result = mysqli_query($db_connect, $sql);
-                if ($query_result) {
-
-                    foreach($query_result as $rand){            
-                        if(empty($rand))
-                            $rand_invs = 0;
-                        else
-                            $rand_invs = $rand['rand_inv'];
-
-                    }
-                    return $rand_invs + 1;
-                }
-        } else {
-            die('Query problem' . mysqli_error($db_connect));
-        }
-    }
-
-//****#####--Admin Function Start--####***  
-    public function fetchRows($sql) {
-        $db_connect = $this->__construct();
-        $arr = array();
-        if (mysqli_query($db_connect, $sql)) {
-            $res = mysqli_query($db_connect, $sql);
-            while ($row = mysqli_fetch_array($res)) {
-                $arr[] = $row;
-            }
-            return $arr;
-        } else {
-            die('Query problem' . mysqli_error($db_connect));
-        }
-    }
-
-//****#####--Common Method Function End--####***
-    public function execute_query($sql) {
-        $db_connect = $this->__construct();
-
-        if (mysqli_query($db_connect, $sql)) {
-            $query_result = mysqli_query($db_connect, $sql);
-            return $query_result;
-        } else {
-            die('Query problem' . mysqli_error($db_connect));
-        }
-    }
-
-    public function getTotalRows($sql) {
-        $db_connect = $this->__construct();
-
-        if (mysqli_query($db_connect, $sql)) {
-            $res = mysqli_query($db_connect, $sql);
-            $NUM = mysqli_num_rows($res);
-            return $NUM;
-        } else {
-            die('Query problem' . mysqli_error($db_connect));
-        }
-    }
 
 //****#####--Admin Login Function Start--####***
 
@@ -148,7 +51,7 @@ class Admin {
                 $_SESSION['branch_id'] = $row['branch_id'];
                 $_SESSION['unique_id'] = $row['unique_id'];
                 $_SESSION['access_permission'] = $row['access_permission'];
-                header('Location: inventory.php');
+                header('Location: welcome.php');
             } if($row['access_permission'] = 1){
                 $message= "Sorry You have no Access to this site";
                 return $message;
