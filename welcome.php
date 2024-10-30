318acf78b54270cdcda05beca086d6cc75fda085
Abdullah Awwal <contact@abdullahawwal.com>
2024-10-30 07:28:29 +0000

    page created

---
+++ welcome.php
@@ -0,0 +1,17 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>welcome</title>
+</head>
+<body>
+    <div class="welcome">
+        <h1>Congratulation</h1>
+        <h3>Hello Mr <?php ?> You have successfully logged in.</h3>
+    </div>
+</body>
+</html>
